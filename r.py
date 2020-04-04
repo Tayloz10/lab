046ebb3d6bbe49166fa3e44e3c4a10bce3f1e6fa
Tayloz10 <59981271+Tayloz10@users.noreply.github.com>
2020-04-04 16:21:02 -0600

    Add files via upload

--- r.py
+++ r.py
@@ -6,20 +6,20 @@ class Total:
 
     def print_info(self):
         print("TOTAL COST")
-        for i in range(2):
-            print("{} {} @ ${} =${}".format(self.name[i], self.quantity[i], self.price[i], (self.price[i] * self.quantity[i])))
-            print("Total: ${}".format(self.price[i] * self.quantity[i]))
+        for i in range(0,2):
+            print("{} {} @ ${} = ${}".format(self.name[i], self.quantity[i], self.price[i], (self.price[i] * self.quantity[i])))
+        print("Total: ${}".format((self.price[0] * self.quantity[0]) + (self.price[1] * self.quantity[1]))
 
 if __name__ == "__main__":
-    price_t = Total()
-    price_t.set_name = []
-    price_t.set_price = []
-    price_t.set_quantity = []
+    set_name = []
+    set_price = []
+    set_quantity = []
+    price_t = Total(set_name,  set_quantity, set_price)
     i = 1
     while i <= 2:
         print("Item{}" .format(i))
-        price_t.set_name = input("Enter the item name:\n")
-        price_t.set_price = int(input("Enter the item price:\n"))
-        price_t.set_quantity = int(input("Enter the item quantity:\n"))
+        set_name.append(input("Enter the item name:\n"))
+        set_price.append(int(input("Enter the item price:\n")))
+        set_quantity.append(int(input("Enter the item quantity:\n")))
         i += 1
 price_t.print_info()
